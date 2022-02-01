2468f520cf883dbf6cb48cad02450e1e209851f2
austinkelsay <austinkelsay@yahoo.com>
2022-01-31 19:26:36 -0700

    Added title container and new font

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -66,7 +66,7 @@ function BottomTabNavigator() {
         name="TabOne"
         component={TabOneScreen}
         options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'Tab One',
+          title: 'Bitcoin Knowledge Bot',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Pressable
@@ -88,7 +88,7 @@ function BottomTabNavigator() {
         name="TabTwo"
         component={TabTwoScreen}
         options={{
-          title: 'Tab Two',
+          title: 'Bitcoin Datasets',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,19 @@
 import { StyleSheet } from 'react-native';
-
+import { MonoText } from '../components/StyledText';
 import EditScreenInfo from '../components/EditScreenInfo';
+import MessageWindow from '../components/MessageWindow';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <View style={styles.titleContainer}>
+        <MonoText style={styles.title}>Bitcoin Knowledge Bot</MonoText>
+        <MonoText style={styles.subTitle}>A question & answer AI bot that also suggests articles/podcasts <br/> Powered by GPT-3 and trained on an open source dataset of established Bitcoin knowledge</MonoText>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      </View>
+      <MessageWindow path='/screens/TabOneScreen.tsx' />
     </View>
   );
 }
@@ -18,15 +22,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+  titleContainer: {
+    alignItems: 'center',
     justifyContent: 'center',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subTitle: {
+    fontSize: 15,
+    textAlign: 'center',
   },
   separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    marginVertical: 10,
+    height: 2,
+    width: '150%',
   },
 });
